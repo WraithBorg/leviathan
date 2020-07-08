4d90440dd516d80ea1f2a9a7888c07e18b24afd2
Thanos <2325098522@qq.com>
2020-07-08 13:01:24 +0800

    客服数据结构调整

--- src/main/java/com/io/hydralisk/constant/PageConst.java
+++ src/main/java/com/io/hydralisk/constant/PageConst.java
@@ -9,6 +9,7 @@ public class PageConst {
     public static final String ABOUT_US = WEB_HOST+"/h5/pages/html/aboutus";//关于页面
     public static final String PRODUCT_SHOW = WEB_HOST+"/h5/pageb2c/b2c_product/show?id=";//显示商品
     public static final String PRODUCT_LIST_4CATEGORY = WEB_HOST+"/h5/pageb2c/b2c_product/list?catid=";//根据品项类别显示商品列表
+    public static final String KEFU_INDEX = WEB_HOST+ "/h5/pages/kefu/index";//客服页面
 
 
 
--- src/main/java/com/io/hydralisk/controller/KeFuController.java
+++ src/main/java/com/io/hydralisk/controller/KeFuController.java
@@ -1,19 +1,19 @@
 package com.io.hydralisk.controller;
 
-import com.io.hydralisk.constant.CConstant;
+import com.io.hydralisk.constant.PageConst;
 import com.io.hydralisk.convert.KeFuLogConvert;
 import com.io.hydralisk.domain.KeFuLogInfo;
 import com.io.hydralisk.domain.UserInfo;
 import com.io.hydralisk.dto.KeFuLogDTO;
 import com.io.hydralisk.mapper.KeFuLogMapper;
 import com.io.hydralisk.mapper.UserInfoMapper;
+import com.io.hydralisk.result.MsgResult;
 import com.io.hydralisk.service.usb.KeFuLogService;
 import com.io.hydralisk.util.CCommonUtils;
 import com.io.hydralisk.vo.KeFuLogVO;
 import com.io.hydralisk.vo.ResultVO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -25,7 +25,7 @@ import java.util.Map;
 /**
  * 客服
  */
-@RequestMapping("/kefu")
+
 @RestController
 public class KeFuController {
     @Resource
@@ -36,23 +36,23 @@ public class KeFuController {
     private KeFuLogService keFuLogService;
     @Resource
     private UserInfoMapper userInfoMapper;
+
     /**
      * 获取客服记录
      */
-    @GetMapping("/mykefu")
-    public Object mykefu() {
+    @GetMapping("/kefu/mykefu")
+    public MsgResult mykefu() {
         List<KeFuLogInfo> list = keFuLogService.getMyLog();
         List<KeFuLogVO> vos = keFuLogConvert.getKeFuLogVOS(list);
-
         Map data = CCommonUtils.ofMap("list", vos);
-        return new ResultVO(data, CConstant.WEB_HOST + "/h5/pages/kefu/index");
+        return MsgResult.doneUrl(data, PageConst.KEFU_INDEX);
     }
 
     /**
      * 保存客服记录
      */
-    @PostMapping("/savelog")
-    public Object saveLog(KeFuLogDTO keFuLogDTO) {
+    @PostMapping("/kefu/savelog")
+    public Object MsgResult(KeFuLogDTO keFuLogDTO) {
         UserInfo defaultUser = userInfoMapper.getDefaultUser();
         KeFuLogInfo info = new KeFuLogInfo();
         info.setUserId(defaultUser.getId());
@@ -60,7 +60,7 @@ public class KeFuController {
         info.setStatus(1);
         info.setContent(keFuLogDTO.getContent());
         keFuLogMapper.insert(info);
-        return new ResultVO(new ArrayList<>(), CConstant.WEB_HOST + "/h5/pages/kefu/index");
+        return new ResultVO(new ArrayList<>(), PageConst.KEFU_INDEX);
     }
 
 }
