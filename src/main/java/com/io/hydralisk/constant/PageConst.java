cbac31609d8ddb0ced2c5f9592251bddfa0a7fd3
Thanos <2325098522@qq.com>
2020-07-08 11:52:11 +0800

    收藏商品 页面改造优化

--- src/main/java/com/io/hydralisk/constant/PageConst.java
+++ src/main/java/com/io/hydralisk/constant/PageConst.java
@@ -7,6 +7,7 @@ public class PageConst {
     public static final String WEB_HOST = "http://localhost:8080/";
     public static final String CATEGORY_INDEX = WEB_HOST + "/h5/pageb2c/b2c_category/index";//商品类别首页
     public static final String ABOUT_US = WEB_HOST+"/h5/pages/html/aboutus";//关于页面
+    public static final String PRODUCT_SHOW = WEB_HOST+"/h5/pageb2c/b2c_product/show?id=";//显示商品
 
 
 
--- src/main/java/com/io/hydralisk/controller/ItemFavController.java
+++ src/main/java/com/io/hydralisk/controller/ItemFavController.java
@@ -1,11 +1,10 @@
 package com.io.hydralisk.controller;
 
-import com.io.hydralisk.constant.CConstant;
+import com.io.hydralisk.constant.PageConst;
 import com.io.hydralisk.domain.UserInfo;
 import com.io.hydralisk.mapper.UserInfoMapper;
+import com.io.hydralisk.result.MsgResult;
 import com.io.hydralisk.service.usb.UserFavItemService;
-import com.io.hydralisk.vo.ResultVO;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +14,6 @@ import javax.annotation.Resource;
  * 收藏商品
  */
 @RestController
-@RequestMapping("/itemfav")
 public class ItemFavController {
     @Resource
     private UserFavItemService userFavInfoService;
@@ -25,11 +23,11 @@ public class ItemFavController {
     /**
      * 收藏商品
      */
-    @GetMapping("/toggle")
-    public Object toggle(String objectid) {
+    @RequestMapping("/item/itemfav")
+    public MsgResult toggle(String objectid) {
         UserInfo defaultUser = userInfoMapper.getDefaultUser();
         String method = userFavInfoService.favItem(defaultUser.getId(), objectid);
-        return new ResultVO(method, CConstant.WEB_HOST + "/h5/pageb2c/b2c_product/show?id=" + objectid);
+        return MsgResult.doneUrl(method, PageConst.PRODUCT_SHOW + objectid);
     }
 
 
